2b15cdf737e0849ffa6cf6ba0cd82f137d5fa283
egotch <egotkowski@gmail.com>
2018-11-15 00:36:10 -0600

    Updates on main blog view

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
 ]
+
+#Add in the static fields for CSS and other things
+urlpatterns += staticfiles_urlpatterns()
