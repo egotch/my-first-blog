51b00b820ceacb9ce3eb1fb8d7e9bb99fb2b21ff
egotch <egotkowski@gmail.com>
2018-11-18 11:15:49 -0600

    Creating new posts, editing posts Minor security update

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,9 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>', views.post_detail, name='post_detail')
+    path('post/<int:pk>', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
 
 #Add in the static fields for CSS and other things
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import  Post
-
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -18,4 +18,41 @@ def post_detail(request, pk):
         'post': post,
     }
     return render(request, 'blog/post_detail.html', context)
+
+def post_new(request):
+
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+    else:
+        form = PostForm()
+    context = {
+        'form': form,
+    }
+
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.published_date = timezone.now()
+        post.summary_text = ' '.join(post.text.split(' ')[:50])
+        post.save()
+        return redirect('post_detail', pk=post.pk)
+
+    return render(request, 'blog/post_edit.html', context)
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+
 
